287b24832d3124e3511a00a4b719e498a8b0be5c
NightCandle <instkffff@gmail.com>
2020-05-28 01:09:11 +0800

    .

--- blog/themes/paper/source/js/paper.js
+++ blog/themes/paper/source/js/paper.js
@@ -7,6 +7,8 @@ const invertImgs = (function() {
   return invertImgs
 })()
 
+
+
 function Darkmode() {
   this.mask = document.getElementById('darkmode-mask')
   this.imgs = document.getElementsByTagName('img')
@@ -19,7 +21,18 @@ Darkmode.prototype.turnOnDarkmode = function() {
   this.mask.classList.add('darkmode-mask--dark')
   // invert the images color
   // so that they can render with the right color
-  invertImgs(this.imgs)
+  // invertImgs(this.imgs)
+
+  //#6da051
+  let hoverShadow = 'img {filter: invert(100%)} article.post-view__article .article__content a:hover {box-shadow: inset 0 -0.8em 0 #6da051;}.sidebar__link a:hover, .sidebar__archives a:hover, .sidebar__categories a:hover, .sidebar__tags a:hover {box-shadow: inset 0 -0.8em 0 #6da051;}.tocbot .is-active-link::before {background: #6da051;}.sidebar__link, .sidebar__archives, .sidebar__categories, .sidebar__tags {padding: 0 0 0 1em;border-left: 2px solid #6da051;margin: 1.6rem 0 1.6rem 1rem;} .location-bar .text .active:hover {box-shadow: inset 0 -0.8em 0 #6da051;}.location-bar .text .active {pointer-events: all;text-decoration: none;box-shadow: inset 0 -0.3em 0 #6da051;transition: box-shadow 0.5s cubic-bezier(0.71, 0, 0, 0.99);} .posts-item:hover h2 a {box-shadow: inset 0 -0.8em 0 #6da051} .posts-item__title a {text-decoration: none;box-shadow: inset 0 -0.3em 0 #6da051;transition: box-shadow 0.5s cubic-bezier(0.71, 0, 0, 0.99) '
+  let style = document.createElement('style');
+  style.setAttribute("id","DarkMode")
+  if (style.styleSheet) {
+    style.styleSheet.cssText = hoverShadow;
+  } else {
+    style.appendChild(document.createTextNode(hoverShadow));
+  }
+  document.getElementsByTagName('head')[0].appendChild(style);
 
   // set <meta name="theme-color" content="xxxx">
   this.metaThemeColorCatch = this.metaThemeColor.content
@@ -27,8 +40,9 @@ Darkmode.prototype.turnOnDarkmode = function() {
 }
 Darkmode.prototype.turnOffDarkmode = function() {
   this.mask.classList.remove('darkmode-mask--dark')
-  invertImgs(this.imgs)
+  // invertImgs(this.imgs)
   this.metaThemeColor.content = this.metaThemeColorCatch
+  document.getElementById('DarkMode').remove()
 }
 
 window.addEventListener('DOMContentLoaded', () => {
